157dc206fe6308f4b64059287109682c97a48333
HoneyRao-47 <honey.23cs047@sode-edu.in>
2026-07-04 18:25:19 +0530

    COMPARISON_OPERATOR

---
+++ WHERE _CLAUSE_WITH _COMPARISON _OPERATOR.sql
@@ -0,0 +1,12 @@
+-- WHERE CLAUSE
+#COMPARISION OPERATOR
+SELECT * FROM employee_salary WHERE first_name = 'Leslie';
+SELECT * FROM employee_salary WHERE salary>50000;
+select * FROM employee_demographics WHERE gender!='Female';
+SELECT * FROM employee_demographics WHERE birth_date > '1985-01-01';
+
+
+
+
+
+
